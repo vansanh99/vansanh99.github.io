4fdd2399b05cb49b4d6f740d83bef8ee8f812e00
vansanh99 <sanhpham417@gmail.com>
2023-05-20 12:31:17 +0700

    change values

--- index_files/functions.js
+++ index_files/functions.js
@@ -52,6 +52,6 @@ function timeElapse(date){
 	}
 	seconds = Math.floor(seconds % 60);
 	seconds = (seconds + "").replace(/\.\d*/, "");
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
+	var result = "<span class=\"digit\">" + days + "</span> ngày <span class=\"digit\">" + hours + "</span> giờ <span class=\"digit\">" + minutes + "</span> phút <span class=\"digit\">" + seconds + "</span> giây";
 	$("#clock").html(result);
 }
