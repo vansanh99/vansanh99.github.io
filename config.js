a67cc399f000de2a255242aeafd5c7efd749b49c
vansanh99 <sanhpham417@gmail.com>
2023-05-20 12:06:56 +0700

    change

---
+++ config.js
@@ -0,0 +1,21 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Gửi người yêu dấu của anh,",
+        "Hoa nở đỏ, tình trao",
+        "Tương tư trọn vẹn, không xa cách",
+        "Em là duy nhất, anh yêu mãi mãi",
+        "Trái tim anh trao, tình ngọt ngào không phôi pha",
+        "Yêu em mãi mãi..."
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2023-05-01T00:00:00",
+    // 双方的名字
+    names: ["Linh", "Sánh"]
+}
